8d6ad4c42baf0cf4cf343e0533613f2e1980108c
user2 <belairemusic@gmail.com>
2020-08-02 20:51:52 -0700

    use Meals in App, final styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React, { Suspense } from 'react';
+import Meals from './components/Meals';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import './App.css';
 
 export default function App() {
   return (
     <Suspense fallback="">
-      <p style={{fontFamily:'Super Grotesk Pro Bold'}}>hello, world!</p>
+      <Header title="See what we're serving up next week"/>
+      <main>
+        <section className="description">
+          <h3>Nutrition is on the menu</h3>
+          <p>Our meals are designed by nutritionists and crafted by local chefs. Free of gluten, refined sugars or dairy* and responsibly sourced.</p>
+          <p className="finePrint">*Meals are crafted in kitchens where gluten, dairy, and other allergens are present, and thus are not certified gluten-free, dairy or other allergen-free.</p>
+        </section>
+        <Meals/>
+      </main>
+      <Footer name={'Territory Foods'}/>
     </Suspense>
   );
 }
